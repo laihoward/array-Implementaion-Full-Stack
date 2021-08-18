7860a91b8fe6bd91142cb1b88051aaadaada56b2
Pin Hao Lai <howardlai30425@gmail.com>
2021-08-18 21:53:16 +0800

    requirements.txt

--- run.py
+++ run.py
@@ -1,10 +1,29 @@
-# -*- coding: UTF-8 -*-
-from app import app
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from bubbleSort import BubbleSort
+
+app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/')
-def index():
-    return 'Flask API start'
+@app.route('/add_array', methods=['POST'])
+def pushdata():
+    data = request.get_json()
+    func = data['func']
+    array=data['array']
+    if func == "push":
+        newdata=int(data['newdata'])
+        array.append(newdata)
+    elif func == "clear":
+        array = []
+    elif func == "delete":
+        newdata=int(data['newdata'])
+        array.pop(newdata)
+    elif func == "sort":
+        BubbleSort(array)
+    
+    return jsonify(array)
 
 
 if __name__ == '__main__':
